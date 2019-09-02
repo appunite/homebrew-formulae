39bde013e036bd8770f64f7eb48f7f41ac04abe9
Lukasz Kasperek <luk.kasperek@gmail.com>
2019-09-03 00:25:35 +0200

    update version to 0.1.3

--- Formula/palmyra.rb
+++ Formula/palmyra.rb
@@ -3,8 +3,8 @@ class Palmyra < Formula
   homepage "https://github.com/appunite/Palmyra"
   url "https://github.com/appunite/Palmyra",
     :using => :git,
-    :tag => "0.1.0",
-    :revision => "9395137257d2831c1b5ef6134be17c40263cae37"
+    :tag => "0.1.3",
+    :revision => "e72c620c89699b88b4d0275a3a4a91e5af5384c2"
   head "https://github.com/appunite/Palmyra.git"
 
   depends_on :xcode => ["11.0", :build]
